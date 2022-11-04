3200be76f0b9125aca39f559e71e5369be986bde
MinhajSadik <MinhajoSadik@gmail.com>
2022-11-04 11:33:10 +0600

    [ADDED] java variable

--- playground.java
+++ playground.java
@@ -1,5 +1,7 @@
 class Playground{  
     public static void main(String args[]){  
-     System.out.println("First Java!");  
+        String name;
+        name = "Minhaj";
+     System.out.println(name);  
     }  
 }
